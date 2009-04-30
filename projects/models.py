a382445adb1d1e817af5092e459997c8926a65a8
Johan Haals <johan.haals@gmail.com>
2009-04-30 16:00:54 +0200

    Added some help_text.

--- projects/models.py
+++ projects/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 class OurProjects(models.Model):
     project_name = models.CharField(max_length=30)
-    project_description = models.CharField(max_length=30)
-    project_url = models.CharField(max_length=30)
+    project_description = models.CharField(max_length=30, help_text='Just a few words about the project.')
+    project_url = models.CharField(max_length=30, help_text='example: http://site.scriptdev.se')
     def __unicode__(self):
         return self.project_name
 
