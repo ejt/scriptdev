c8d4022fd73e354bc56b1da5df40da7ec1903c8f
Johan Haals <johan.haals@gmail.com>
2009-05-03 11:10:34 +0200

    Projects -> index.html

--- views.py
+++ views.py
@@ -1,10 +1,10 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render_to_response
-
+from projects.models import Project
 def index(request):
     return render_to_response('index.html',{
-        # Uncomment me when ready
-        #'project_list': Project.objects.all(),
+
+        'project_list': Project.objects.all(),
 
         'user_list': User.objects.all(),
     })
