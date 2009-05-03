a83927def341c760e92bfef65c28b6844a421302
Marcus Fredriksson <drmegahertz@gmail.com>
2009-05-03 22:29:26 +0200

    Fixes project image upload and display

--- projects/models.py
+++ projects/models.py
@@ -1,12 +1,11 @@
 from django.db import models
-
-# Create your models here.
+import os
 
 class Project(models.Model):
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=30, help_text='Just a few words about the project.')
     url = models.URLField(verify_exists=False, help_text='example: http://site.scriptdev.se')
-    img = models.ImageField(upload_to='/WE_NEED_OS_Join_thing_here/', height_field='200', width_field='287')
+    img = models.ImageField(upload_to=os.path.join('img', 'projects'))
     def __unicode__(self):
         return self.name
 
--- settings.py
+++ settings.py
@@ -52,7 +52,8 @@ MEDIA_ROOT = os.path.join(PROJECT_PATH, 'media')
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = ''
+# Err, change me
+MEDIA_URL = 'http://localhost:8000/media/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
