bcd108b3083e71ae44454e8ad140422fed0ba65d
Marcus Fredriksson <drmegahertz@gmail.com>
2009-05-03 14:16:43 +0200

    Added translation strings and swedish translation
    Of course we want swedish fieldnames and such in
    the admin interface! :)
    
    Use `django-admin.py compilemessages` in the
    application directory to compile the translated
    strings for usage.

--- profiles/models.py
+++ profiles/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
 
 class Profile(models.Model):
-    user = models.ForeignKey(User, unique = True)
-    roles = models.CharField(max_length = 50, blank = True, help_text = 'Member roles, separated by spaces, eg. "programmer designer"')
-    twitter_username = models.CharField(max_length = 15, blank = True, help_text = 'Enter username to display latest tweet on the front page')
+    user = models.ForeignKey(User, unique = True, verbose_name = _('user'))
+    roles = models.CharField(_('roles'), max_length = 50, blank = True, help_text = _('Member roles, eg. "Programmer, Webdesigner"'))
+    twitter_username = models.CharField(_('twitter username'), max_length = 15, blank = True, help_text = _('Enter username to display latest tweet on the front page'))
+
+    class Meta:
+        verbose_name = _('profile')
+        verbose_name_plural = _('profiles')
 
     def __unicode__(self):
         return '%s\'s profile' % self.user
