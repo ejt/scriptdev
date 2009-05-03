d05394f9b0e9b4f2c5d914863678b520b191bc1f
Johan Haals <johan.haals@gmail.com>
2009-05-03 18:59:21 +0200

    images can now be uploaded.

--- projects/models.py
+++ projects/models.py
@@ -6,7 +6,7 @@ class Project(models.Model):
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=30, help_text='Just a few words about the project.')
     url = models.URLField(verify_exists=False, help_text='example: http://site.scriptdev.se')
-#    project_img = models.ImageField()
+    img = models.ImageField(upload_to='/WE_NEED_OS_Join_thing_here/', height_field='200', width_field='287')
     def __unicode__(self):
         return self.name
 
