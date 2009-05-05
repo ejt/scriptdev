b88b2bd3e12f2d38875c48ec6dbcd3986eafe916
Marcus Fredriksson <drmegahertz@gmail.com>
2009-05-05 12:47:19 +0200

    Changed to predefined authform, adds contact code
    Replaces the old hardcoded authentication form with
    the predefined AuthenticationForm from
    django.contrib.auth.forms.
    
    Moves login logic from the bundled login-view to our
    index-view.
    
    Adds the real contact form in the template and adds
    the contact form logic.

--- views.py
+++ views.py
@@ -1,10 +1,34 @@
-from django.contrib.auth.models import User
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User
 from projects.models import Project
+from contact_form.forms import ContactForm
 
 def index(request):
+    if request.method == "POST":
+        # User authentication
+        auth_form = AuthenticationForm(data = request.POST)
+
+        if auth_form.is_valid():
+            user = auth_form.get_user()
+
+            if user.is_active:
+                login(request, user)
+
+        # Contact form handling
+        contact_form = ContactForm(request = request, data = request.POST)
+
+        if contact_form.is_valid():
+            contact_form.save()
+    else:
+        auth_form = AuthenticationForm()
+        contact_form = ContactForm(request = request)
+
     return render_to_response('index.html',{
+        'auth_form': auth_form,
+        'contact_form': contact_form,
         'project_list': Project.objects.all(),
-        'user_list': User.objects.all(),
+        'user_list': User.objects.all()
     }, context_instance = RequestContext(request))
