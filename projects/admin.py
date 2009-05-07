bd4f730e951604d9a4d57500f7e7a37aff763617
Marcus Fredriksson <drmegahertz@gmail.com>
2009-05-07 19:21:45 +0200

    Breaks out project images to their own models
    This way we don't get several copies of the same
    image if we assign multiple projects to the same
    file.

--- projects/admin.py
+++ projects/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from models import Project
+from models import Project, Image
 
-admin.site.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'url')
+
+admin.site.register(Image)
+admin.site.register(Project, ProjectAdmin)
--- projects/models.py
+++ projects/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 import os
 
+class Image(models.Model):
+    title = models.CharField(max_length = 30)
+    image = models.ImageField(upload_to=os.path.join('img', 'projects'))
+
+    def __unicode__(self):
+        return self.title
+
 class Project(models.Model):
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=30, help_text='Just a few words about the project.')
     url = models.URLField(verify_exists=False, help_text='example: http://site.scriptdev.se')
-    img = models.ImageField(upload_to=os.path.join('img', 'projects'))
+    image = models.ForeignKey(Image)
+
     def __unicode__(self):
         return self.name
-
